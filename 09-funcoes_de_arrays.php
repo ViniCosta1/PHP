be7b2a79ba8ccbd19c7b2da4bbdc05cca1455101
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-10 00:38:20 -0300

    Add archive - "Funções de Arrays"

---
+++ 09-funcoes_de_arrays.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    * is_array($array) = verificar se uma determinada variável é um array
+    * in_array($valor, $array) = verifica se um determinado valor existe em alguma posição da array
+    * array_keys($array) = retorna um novo array com as chaves do array passado como parâmetro
+    * array_values($array) = retorna um novo array com os valores do array passado como parâmetro
+    * array_merge ($array1, $array2) = agrega o conteúdo de dois arrays
+    * array_pop($array) = exclui a ultima posição do array
+    * array_shift($array) = exclui a primeira posição do array
+    * array_unshift($array, "valor") = adiciona um ou mais elementos no início da array
+    * array_push($array, "valor", "valor") = Adiciona um ou mais elementos no final da array
+    * array_combine($keys, $values) = Mescla os dois arrays passados
+    * array_sum() = Calcula a soma dos elementos de uma array
+    * explode("/", "20/03/2005") = Transforma string em uma array
+    * implode("-", $array) = Transforma array em string
+*/
+?>
