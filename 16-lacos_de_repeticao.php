d2428b4136fd992808468a1cbb634cf0e47c2651
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-16 06:45:44 -0300

    Added file of while and do while

---
+++ 16-lacos_de_repeticao.php
@@ -0,0 +1,19 @@
+<?php
+// * Laços de Repetição While & Do While
+// * While
+$contador = 1;
+while ($contador <= 10):
+    echo "Contador é $contador<br>";
+    $contador++;
+endwhile;
+
+echo "<hr>";
+
+
+// * Do while
+$contador = 1;
+do {
+    echo "Contador é $contador<br>";
+    $contador++;
+} while ($contador <= 10);
+?>
