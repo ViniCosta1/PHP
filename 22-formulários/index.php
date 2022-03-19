839beba23c77e8dec1e8287809a36a47e6e13777
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-19 00:41:40 -0300

    Starting the validate's functions

--- 22-formulários/index.php
+++ 22-formulários/index.php
@@ -1,10 +1,39 @@
 <html>
 <body>
 
-<form action="dados.php" method="POST">
-    Nome: <input type="text" name="nome" id=""><br>
+<?php
+/*  
+    * VALIDAÇÕES
+    * Funções "filter_input - Filter_var"
+    * FILTER_VALIDATE_INT
+    * FILTER_VALIDATE_EMAIL
+    * FILTER_VALIDATE_FLOAT
+    * FILTER_VALIDATE_IP
+    * FILTER_VALIDATE_URL
+*/
+?>
+
+<?php
+
+if (isset($_POST['enviar-formulario'])):  // * Se Existe
+    // echo "Enviou <br>";
+    // var_dump($_POST);
+    $erros = array();
+    $idade = $_POST['idade'];
+
+    if ($idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
+        echo "é um inteiro";
+    endif;
+endif;
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    Idade: <input type="text" name="idade" id=""><br>
+    Peso: <input type="text" name="peso" id=""><br>
     Email: <input type="text" name="email" id=""><br>
-    <input type="submit" value="Enviar" name="enviar">
+    IP: <input type="text" name="ip" id=""><br>
+    URL: <input type="text" name="url" id=""><br>
+    <button type="submit" name = "enviar-formulario"> Enviar </button> <br>
 </form>
 
 </body>
