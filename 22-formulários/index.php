6af765b0895611431dc8c03f6f679bd980d5e759
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-19 00:23:34 -0300

    Added method $_POST

---
+++ 22-formulários/index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="dados.php" method="POST">
+    Nome: <input type="text" name="nome" id=""><br>
+    Email: <input type="text" name="email" id=""><br>
+    <input type="submit" value="Enviar" name="enviar">
+</form>
+
+</body>
+</html>
